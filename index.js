c155cd7336d136af212dbda15dfddae92448d291
Michael Johnson <michaelbojohnson@gmail.com>
2022-03-16 10:07:13 -0500

    commit tree 070929bf08e28b0e9639ac53576c625d257ab2c5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+/*function superbowlWin(record){
+    let winningYear = record.find(superbowlWin)
+    if (winningYear){
+        return record.year
+    }
+
+}*/
+
+function superbowlWin(array){
+    for (const user of array){
+        if(user.result === "W"){
+            return user.year;
+        }
+    }
+}
